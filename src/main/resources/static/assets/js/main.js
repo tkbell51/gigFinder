4d2a60de474867783549bb93d2f26101cafe2885
tkbell51 <tkbell51@gmail.com>
2017-12-23 09:01:35 -0500

    fixed styling bugs

--- src/main/resources/static/assets/js/main.js
+++ src/main/resources/static/assets/js/main.js
@@ -1,7 +1,7 @@
 (function($){
     "use strict";
 
-    $(".valign-wrapper i, #hover-content i, table i, .prefix, .date, .month, .card-panel .btn-floating i").addClass("orange-gradient-text");
+    $(".valign-wrapper i, #hover-content i, table i, .prefix, .date, .month, .card-panel .btn-floating i, .appliedNum").addClass("orange-gradient-text");
     $(".btn, .btn-floating, .btn-large").css("border-radius", "20px");
     $(".btn, .btn-floating, .btn-large, .divider, .hireBtn").addClass("orange-gradient");
     $(".card-panel .btn-floating").css("background", "#FFF");
