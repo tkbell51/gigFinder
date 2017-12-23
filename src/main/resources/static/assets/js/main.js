1c5a17015bd5c3f6e43fd9065e7e6f39ac649c13
tkbell51 <tkbell51@gmail.com>
2017-12-23 01:45:44 -0500

    updated style

--- src/main/resources/static/assets/js/main.js
+++ src/main/resources/static/assets/js/main.js
@@ -1,7 +1,10 @@
 (function($){
     "use strict";
-    
-    $(".btn, .btn-floating, .btn-large").addClass("orange accent-4");
+
+    $(".valign-wrapper i, #hover-content i, table i, .prefix, .date, .month, .card-panel .btn-floating i").addClass("orange-gradient-text");
+    $(".btn, .btn-floating, .btn-large").css("border-radius", "20px");
+    $(".btn, .btn-floating, .btn-large, .divider, .hireBtn").addClass("orange-gradient");
+    $(".card-panel .btn-floating").css("background", "#FFF");
 
 
 }(jQuery));
